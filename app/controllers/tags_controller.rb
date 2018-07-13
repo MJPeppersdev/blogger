81ffc0418b3594e772692d8f39d88d0f145e33ce
Michael J Peppers <mcgdevfunk824@protonmail.com>
2018-07-13 00:51:28 -0700

    Create tags_controller.rb

---
+++ app/controllers/tags_controller.rb
@@ -0,0 +1,10 @@
+class TagsController < ApplicationController
+    def index
+        @tags = Article.tag_counts
+    end
+
+    def show
+        @tag = params[:id]
+        @articles = Article.tagged_with(params[:id])
+    end
+end
